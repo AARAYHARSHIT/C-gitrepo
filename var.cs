5cd80f2a154bd643561a2e590295fac11a9179b6
Harshit Pathak <harshitp9045@gmail.com>
2026-05-16 19:20:41 +0530

    Variables and data types in c#
    using class

---
+++ var.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int age = 25;
+        double height = 5.9;
+        char grade = 'A';
+        string name = "John";
+
+        Console.WriteLine(name + " is " + age + " years old.");
+    }
+}
