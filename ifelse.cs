cad39647565da8c308bf3b36b56829bc7a945fdc
Harshit Pathak <harshitp9045@gmail.com>
2026-05-17 19:36:01 +0530

    if_else
    learnt if else statements in c#

---
+++ ifelse.cs
@@ -0,0 +1,18 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int number = 10;
+
+        if (number > 0)
+        {
+            Console.WriteLine("Positive number");
+        }
+        else
+        {
+            Console.WriteLine("Negative number");
+        }
+    }
+}
