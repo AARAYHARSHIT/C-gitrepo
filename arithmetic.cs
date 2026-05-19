ba70f6b4941f480fa88d1933d9d6f4d46041083a
Harshit Pathak <harshitp9045@gmail.com>
2026-05-19 19:07:41 +0530

    arithmetic operators

---
+++ arithmetic.cs
@@ -0,0 +1,9 @@
+int a = 10, b = 5;
+Console.WriteLine(a + b); // 15
+Console.WriteLine(a - b); // 5
+Console.WriteLine(a * b); // 50
+Console.WriteLine(a / b); // 2
+Console.WriteLine(a % b); // 0
+
+a++; // a is now 11
+b--; // b is now 4
