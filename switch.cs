84400e8788dc59712dcc8d8cf68e5e5eaff52fe9
Harshit Pathak <harshitp9045@gmail.com>
2026-05-20 16:53:34 +0530

    switchcase
    in c#

---
+++ switch.cs
@@ -0,0 +1,19 @@
+int day = 4;
+switch (day)
+{
+    case 1:
+        Console.WriteLine("Monday");
+        break;
+    case 2:
+        Console.WriteLine("Tuesday");
+        break;
+    case 3:
+        Console.WriteLine("Wednesday");
+        break;
+    case 4:
+        Console.WriteLine("Thursday");
+        break;
+    default:
+        Console.WriteLine("Another day");
+        break;
+}
