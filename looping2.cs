15881707d8f3d517bca4dd7ff3503db31d0a8692
Harshit Pathak <harshitp9045@gmail.com>
2026-05-23 19:16:49 +0530

    Do-while loop
    example code

---
+++ looping2.cs
@@ -0,0 +1,22 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int i = 1;
+
+        /*
+            DO-WHILE LOOP:
+            Executes at least once.
+        */
+
+        do
+        {
+            Console.WriteLine(i);
+
+            i++;
+
+        } while (i <= 5);
+    }
+}
