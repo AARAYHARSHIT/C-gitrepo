13d9f76e4e4923ca30078b3ace21b8aed2fc2a91
Harshit Pathak <harshitp9045@gmail.com>
2026-05-25 16:19:48 +0530

    OOPS
    oops started in c# with constructor program

---
+++ OOPs/constructor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Student
+{
+    public string Name;
+
+    /*
+        CONSTRUCTOR:
+        Automatically called when object is created.
+    */
+
+    public Student(string studentName)
+    {
+        Name = studentName;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Student s1 = new Student("Harshit");
+
+        Console.WriteLine(s1.Name);
+    }
+}
