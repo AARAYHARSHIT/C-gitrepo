efa8a625a2dfdf0ec8db829a8a14d0447fdc5c93
Harshit Pathak <harshitp9045@gmail.com>
2026-05-29 21:27:00 +0530

    File manipulation

---
+++ FileRead/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        /*
+            READ FILE CONTENT
+        */
+
+        string text = File.ReadAllText("data.txt");
+
+        Console.WriteLine(text);
+    }
+}
