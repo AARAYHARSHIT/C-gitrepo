bed7320a74c05da393d143c3e914631f5e568949
Harshit Pathak <harshitp9045@gmail.com>
2026-05-31 13:26:28 +0530

    dictionary in c#

--- dictionary/Program.cs
+++ dictionary/Program.cs
@@ -1,1 +1,26 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        /*
+            DICTIONARY:
+            Stores data in key-value pairs.
+        */
+
+        Dictionary<int, string> students =
+            new Dictionary<int, string>();
+
+        students.Add(101, "Harshit");
+        students.Add(102, "Rahul");
+
+        foreach (var item in students)
+        {
+            Console.WriteLine(
+                item.Key + " : " + item.Value
+            );
+        }
+    }
+}
