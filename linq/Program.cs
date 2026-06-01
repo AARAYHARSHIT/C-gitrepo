c440b6009bdcd25ea81d55897cfd23f8f09405ff
Harshit Pathak <harshitp9045@gmail.com>
2026-06-01 21:17:44 +0530

    basic linq
    example

--- linq/Program.cs
+++ linq/Program.cs
@@ -1,1 +1,22 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = { 10, 20, 30, 40, 50 };
+
+        /*
+            LINQ:
+            Used for querying/filtering data.
+        */
+
+        var result = numbers.Where(n => n > 25);
+
+        foreach (int num in result)
+        {
+            Console.WriteLine(num);
+        }
+    }
+}
