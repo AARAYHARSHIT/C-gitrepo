0fd5ee9b2fe59a8e73c3d7cf26cc09f009c7e77b
Harshit Pathak <harshitp9045@gmail.com>
2026-06-05 20:20:50 +0530

    generic class
    code

--- generic/Program.cs
+++ generic/Program.cs
@@ -1,1 +1,25 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Box<T>
+{
+    public T Value;
+
+    public void Show()
+    {
+        Console.WriteLine(Value);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Box<int> numberBox = new Box<int>();
+        numberBox.Value = 100;
+        numberBox.Show();
+
+        Box<string> textBox = new Box<string>();
+        textBox.Value = "Hello";
+        textBox.Show();
+    }
+}
--- generic/obj/Debug/net10.0/generic.AssemblyInfo.cs
+++ generic/obj/Debug/net10.0/generic.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("generic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c153ca9151c9f48c132130a155bb1c8897f99b3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d4bbaf3e6c03a4720641d94e5d9091682492b7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("generic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("generic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
