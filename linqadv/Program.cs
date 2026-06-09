5351621b9791d46c8fdac3f9d438385a7ef78b41
Harshit Pathak <harshitp9045@gmail.com>
2026-06-09 21:10:26 +0530

    LINQ ADVANced filtering
    code commit

--- linqadv/Program.cs
+++ linqadv/Program.cs
@@ -1,1 +1,21 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int[] marks =
+        {
+            45, 90, 78, 34, 88, 99
+        };
+
+        var toppers =
+            marks.Where(m => m >= 80);
+
+        foreach (int m in toppers)
+        {
+            Console.WriteLine(m);
+        }
+    }
+}
