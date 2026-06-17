c68ae19b53e6b816860732108454454d3a8ce03c
Harshit Pathak <harshitp9045@gmail.com>
2026-06-17 22:34:39 +0530

    code
    used everywhere in asp.net core

--- depinjection/Program.cs
+++ depinjection/Program.cs
@@ -1,1 +1,27 @@
-﻿Console.WriteLine("Hello, World!");
+﻿interface IMessageService
+{
+    void SendMessage();
+}
+
+class EmailService : IMessageService
+{
+    public void SendMessage()
+    {
+        Console.WriteLine("Email Sent");
+    }
+}
+
+class Notification
+{
+    private IMessageService service;
+
+    public Notification(IMessageService service)
+    {
+        this.service = service;
+    }
+
+    public void Notify()
+    {
+        service.SendMessage();
+    }
+}
